c181631a24760f1fd82b17a77c2479a9ea61f153
Soroush-XYZ <Soroushsadeghian.ss@gmail.com>
2021-04-01 17:53:48 +0430

    Created .env file and updated README

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
 # Crocodile Finance
 
-This is a DApp I made on Tron block chain.
+This a Ponzi DApp I created on Tron blockchain using NodeJS.
+
+<!-- GETTING STARTED -->
+# Getting Started
+
+1- First you need to install required packages.
+* npm
+  ```sh
+  npm install
+  ```
+2- Deploy the Solidity smart contract on Tron Blockchain (Either Mainnet or Testnet).
+
+3- Create a Tron wallet and copy it's privatekey.
+
+3- Create a `.env` file in the project's root directory and enter the following values :
+```sh
+PRIVATEKEY='Your_Private_Key'
+CONTRACTADDRESS='Smart_Contract_Address'
+OWNERADDRESS='Your_Own_Wallet_Address'
+```
+
+4- Run `node crocodile.js` and you should get the app perfectly running.
--- crocodile.js
+++ crocodile.js
@@ -1,6 +1,9 @@
 const TronWeb = require('tronweb');
 const express = require('express');
 const app = express();
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 app.use(express.static(__dirname + '/public'));
 
@@ -9,13 +12,14 @@ const HttpProvider = TronWeb.providers.HttpProvider;
 const fullNode = new HttpProvider("https://api.nile.trongrid.io");
 const solidityNode = new HttpProvider("https://api.nile.trongrid.io");
 const eventServer = new HttpProvider("https://api.nile.trongrid.io");
-const privateKey = "B12DB18A29379C314921238F7D6F915C0711253EAAC702785296D9A83053F6FB"; //Enter a Tron wallet private key.
+const privateKey = process.env.PRIVATEKEY; //Enter a Tron wallet private key.
 const tronWeb = new TronWeb(fullNode,solidityNode,eventServer,privateKey);
 
-let contractAddress = 'TKZFkNV3fzyWakw4D2n1KNp6CuYU7WrDUf' //Enter your smart contract address.
+let contractAddress = process.env.CONTRACTADDRESS //Enter your smart contract address.
 
-let Owner_Address = 'TBwtV7fPJsqMU2QAw6PMzkJViLTBM2VyQo'; //Enter the owners account address.
-const rate = 10;
+let Owner_Address = process.env.OWNERADDRESS; //Enter the owner's account address.
+
+const rate = 10; //Daily ROI.
 
 app.set('view engine', 'ejs');
 
